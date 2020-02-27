51b8eff15eb0e0557136f64602fd295141cd958f
amishra1947github <56932581+amishra1947github@users.noreply.github.com>
2020-02-27 08:43:37 +0530

    Add files via upload

---
+++ adusers inactive since 180 days.ps1
@@ -0,0 +1,5 @@
+﻿
+$Days = (get-date).adddays(-180)
+$days
+# Get-ADUser -properties * -f {LastLogonDate -le $days -and name -notlike 'Lco*' -and name -notlike 'dpo*'} | select name, lastlogondate, Enabled | Format-Table > D:\NotLoggedOnSince180Days.csv
+Get-ADUser -properties * -f {Enabled -le $days -and name -notlike 'Lco*' -and name -notlike 'dpo*'} | select name, Enabled | Format-Table > D:\disabled.csv
---
+++ get Active inactive users.ps1
@@ -0,0 +1,8 @@
+﻿$list = get-content 'D:\A\List.Txt'
+$op = foreach($user in $list){ get-aduser -Filter {samaccountname -eq $user} -Properties enabled | ft name, enabled -HideTableHeaders}
+$op > 'd:\a\o.txt'
+Get-Content 'd:\a\o.txt' | Where { $_ -ne ''} > 'd:\a\samaccountname.txt'
+
+
+
+
